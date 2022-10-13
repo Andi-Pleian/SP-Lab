0e617b39e8e159c2911c7545571adaea65385a92
Pleian Andrei Mihai <69640908+Andi-Pleian@users.noreply.github.com>
2022-10-13 12:12:05 +0300

    Create Image.java
    Added Image class.

---
+++ src/Image.java
@@ -0,0 +1,52 @@
+/**
+ * IMPORTS
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Andi
+ *
+ */
+public class Image {
+	//Private Variables:
+
+	/* Image's name */
+	private String imageName;
+	
+	// Public Functions:
+
+	/**
+	 * @param imageName_arg 
+	 */
+	public void setImageName(String imageName_arg) {
+		this.imageName = imageName_arg;
+	}
+	
+	/**
+	 * @return the name of the Image
+	 */
+	public String getImageName() {
+		return this.imageName;
+	}
+
+	/**
+	 * Constructor for Image class
+	 * 
+	 * @param name_arg => name of the chapter
+	 */
+	public Image(String imageName_arg) {
+		this.imageName = imageName_arg;
+	}
+	
+	/**
+	 * Function that prints the Image's name
+	 */
+	public void print() {
+		System.out.println(this.getImageName());
+	}
+}
+
+/**
+ * END OF FILE
+ */
